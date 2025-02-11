fb03e71e0ff2aa8a19ce7b29b11f9c8f91ab0df4
Kamil Gralewicz <k.gralewicz@gmail.com>
2025-02-11 18:25:39 +0100

    Added entity definition.

--- src/main/java/pl/gralewicz/kamil/java/app/notebook/notebook/repository/entity/NoteEntity.java
+++ src/main/java/pl/gralewicz/kamil/java/app/notebook/notebook/repository/entity/NoteEntity.java
@@ -1,4 +1,52 @@
 package pl.gralewicz.kamil.java.app.notebook.notebook.repository.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "NOTES")
 public class NoteEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+
+    public NoteEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "NoteEntity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
