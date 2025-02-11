a0b33189261962771a340eac9c4cba515438f36c
Kamil Gralewicz <k.gralewicz@gmail.com>
2025-02-11 18:16:56 +0100

    Added mapper for Author.

--- src/main/java/pl/gralewicz/kamil/java/app/notebook/notebook/service/mapper/AuthorMapper.java
+++ src/main/java/pl/gralewicz/kamil/java/app/notebook/notebook/service/mapper/AuthorMapper.java
@@ -1,4 +1,47 @@
 package pl.gralewicz.kamil.java.app.notebook.notebook.service.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import pl.gralewicz.kamil.java.app.notebook.notebook.controller.model.Author;
+import pl.gralewicz.kamil.java.app.notebook.notebook.repository.entity.AuthorEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Component
 public class AuthorMapper {
+
+    private static final Logger LOGGER = Logger.getLogger(AuthorMapper.class.getName());
+
+    public List<Author> fromEntities(List<AuthorEntity> authorEntities) {
+        LOGGER.info("fromEntities()");
+
+        List<Author> authors = new ArrayList<>();
+
+        for (AuthorEntity authorEntity : authorEntities) {
+            Author author = from(authorEntity);
+            authors.add(author);
+        }
+
+        LOGGER.info("fromEntities(...)= ");
+        return authors;
+    }
+
+    public AuthorEntity from(Author author) {
+        LOGGER.info("from(" + author + ")");
+        ModelMapper modelMapper = new ModelMapper();
+        AuthorEntity authorEntity = modelMapper.map(author, AuthorEntity.class);
+        LOGGER.info("from(...)= " + authorEntity);
+        return authorEntity;
+    }
+
+    public Author from(AuthorEntity authorEntity) {
+        LOGGER.info("from(" + authorEntity + ")");
+        ModelMapper modelMapper = new ModelMapper();
+        Author author = modelMapper.map(authorEntity, Author.class);
+        LOGGER.info("from(...)= " + author);
+        return author;
+    }
+
 }
